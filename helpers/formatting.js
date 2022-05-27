5a160a8fd1dcb46b148016e64ad80fbb2fa547fd
Robert Reinhard <robert.reinhard@bukwild.com>
2022-05-27 14:51:12 -0700

    Return id if already formed

--- helpers/formatting.js
+++ helpers/formatting.js
@@ -1,7 +1,13 @@
-// Get the id from a Shoify gid:// style id.  This strips everything but the
+// Get the id from a Shopify gid:// style id.  This strips everything but the
 // last part of the string.  So gid://shopify/ProductVariant/34641879105581
 // becomes 34641879105581
-export var getShopifyId = function(id) {
-	const matches = id.match(/\/(\w+)$/)
+// https://regex101.com/r/3FIplL/3
+export function getShopifyId(id) {
+
+	// Already a simple ID
+	if (String(id).match(/^\d+$/)) return id
+
+	// Return the ID
+	const matches = id.match(/\/([^\/?]+)(?:\?.+)?$/)
 	if (matches) return matches[1]
 }
