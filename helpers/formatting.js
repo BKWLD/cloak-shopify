bc328f3f537f41353f1189e0973180702d0670e5
Robert Reinhard <robert.reinhard@bukwild.com>
2022-05-27 15:04:22 -0700

    Do base-64-ing of gid

--- helpers/formatting.js
+++ helpers/formatting.js
@@ -1,3 +1,5 @@
+import atob from 'atob-lite'
+
 // Get the id from a Shopify gid:// style id.  This strips everything but the
 // last part of the string.  So gid://shopify/ProductVariant/34641879105581
 // becomes 34641879105581
@@ -6,6 +8,10 @@ export function getShopifyId(id) {
 
 	// Already a simple ID
 	if (String(id).match(/^\d+$/)) return id
+
+	// De-base64.  This should only be required when migrating cart ids that were
+	// stored in a cookie, AKA client-side pre Storefront API version 2022-04.
+	if (!id.match(/^gid:\/\//)) id = atob(id)
 
 	// Return the ID
 	const matches = id.match(/\/([^\/?]+)(?:\?.+)?$/)
